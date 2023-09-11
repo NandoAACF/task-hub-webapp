27dba72628130d3a284b2ac28436eaa901a2c0b8
NandoAACF <nando.aacf88@gmail.com>
2023-09-11 13:17:31 +0700

    feat: add create and read feature

--- backend/app/controllers/todo.controller.js
+++ backend/app/controllers/todo.controller.js
@@ -1,2 +1,39 @@
 const db = require("../models");
 const Todo = db.todos;
+
+exports.findAll = (req, res) => {
+    Todo.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving todos.",
+            });
+        });
+};
+
+exports.create = (req, res) => {
+    if (!req.body.title) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+    }
+
+    // req.body untuk mendapatkan data yang dikirimkan melalui body request
+    const todo = new Todo({
+        title: req.body.title,
+        desc: req.body.desc,
+        category: req.body.category,
+        deadline: req.body.deadline,
+    });
+
+    todo.save(todo)
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            res.status(409).send({
+                message: err.message || "Some error occurred while creating the todo.",
+            });
+        });
+};
--- backend/app/routes/todo.routes.js
+++ backend/app/routes/todo.routes.js
@@ -2,5 +2,8 @@ module.exports = (app) => {
     const todos = require("../controllers/todo.controller.js");
     var router = require("express").Router();
 
+    router.get("/", todos.findAll);
+    router.post("/", todos.create);
+
     app.use("/api/todos", router);
 };
