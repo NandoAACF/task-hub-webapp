2974718650df04e53fa22d6fea4747016ef734a8
NandoAACF <nando.aacf88@gmail.com>
2023-09-13 20:59:22 +0700

    feat: add fields for note

--- backend/app/models/note.model.js
+++ backend/app/models/note.model.js
@@ -1,5 +1,13 @@
 module.exports = (mongoose) => {
-    const schema = mongoose.Schema({}, { timestamps: true });
+    const schema = mongoose.Schema(
+        {
+            title: String,
+            desc: String,
+            topic: String,
+            favorite: Boolean,
+        },
+        { timestamps: true }
+    );
     // Convert _id to id
     schema.method("toJSON", function () {
         const { __v, _id, ...object } = this.toObject();
