32f9d8611a0d6e485871569c37e36d89bd043159
Joy Ivan <joyivan6@gmail.com>
2023-09-13 19:31:34 +0700

    fix: add styling to view template file

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/TaskHub",
+    url: "mongodb+srv://joyivan6:fireflies777@cluster0.modvrhk.mongodb.net/?retryWrites=true&w=majority"
+    // url: "mongodb://localhost:27017/TaskHub",
 };
