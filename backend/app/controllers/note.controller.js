dae4586cfd92994adb85842dc5f148b25c737e31
NandoAACF <nando.aacf88@gmail.com>
2023-09-14 10:42:56 +0700

    feat: add findAll notes

--- backend/app/controllers/note.controller.js
+++ backend/app/controllers/note.controller.js
@@ -1,2 +1,14 @@
 const db = require("../models");
 const Note = db.notes;
+
+exports.findAll = (req, res) => {
+    Note.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving notes.",
+            });
+        });
+};
--- backend/app/routes/note.routes.js
+++ backend/app/routes/note.routes.js
@@ -2,5 +2,7 @@ module.exports = (app) => {
     const notes = require("../controllers/note.controller.js");
     var router = require("express").Router();
 
+    router.get("/", notes.findAll);
+
     app.use("/api/notes", router);
 };
