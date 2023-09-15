2aa4005ec8e738e5bcbeb43593115f2c20e7ca2e
Joy Ivan <joyivan6@gmail.com>
2023-09-15 10:25:14 +0700

    feat: add login functionality

--- backend/app/controllers/user.controller.js
+++ backend/app/controllers/user.controller.js
@@ -1,17 +1,25 @@
 const db = require("../models");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const User = db.user;
 
 exports.register = (req, res) => {
-    if (!req.body.email || !req.body.username || !req.body.password) {
+
+    const {username, email, password} = req.body;
+    if (!email || !username || !password) {
         res.status(400).send({ message: "Content can not be empty!" });
         return;
     }
 
+    //Melakukan hashing terhadap password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
     // req.body untuk mendapatkan data yang dikirimkan melalui body request
     const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
+        username: username,
+        email: email,
+        password: hashedPassword,
     });
 
     user.save(user)
@@ -23,6 +31,36 @@ exports.register = (req, res) => {
                 message: err.message || "Some error occurred while registering.",
             });
         });
+};
+
+exports.login = async (req, res) => {
+    try {
+        const user = await User.findOne({email: req.body.email});
+        if(!user) return res.status(404).send("User not found");
+
+        const isPwdCorrect = await bcrypt.compareSync(req.body.pwd, user.pwd);
+        if(!isPwdCorrect) return res.status(400).send('Wrong password');
+        
+        //Membuat token yang terdiri dari dua access token dan refresh token
+        const accessToken = jwt.sign({id: user.id}, process.env.AUTH_ACCESS_TOKEN, {expiresIn: '10s'});
+        const refreshToken = jwt.sign({id: user.id}, process.env.AUTH_REFRESH_TOKEN, {expiresIn: '1h'});
+        
+        const data = {
+            user: user,
+            accessToken: accessToken,
+            refreshToken: refreshToken
+        };  
+
+        return res.status(200).json({
+            message: "You are authenticated",
+            data: data,
+        });
+
+    } catch(err) {
+        return res.status(500).send({
+            message:err.message
+        });
+    }
 };
 
 exports.findOne = (req, res) => {
