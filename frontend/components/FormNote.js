bbd3d29a624c57ca3467f207f51731c44c41b718
Joy Ivan <joyivan6@gmail.com>
2023-11-03 22:46:50 +0700

    feat: make FormNote for create and update handling

---
+++ frontend/components/FormNote.js
@@ -0,0 +1,89 @@
+import Button from "@/components/Button";
+import { AuthContext } from "@/utils/context/AuthContext";
+import useAxios from "@/utils/hooks/useAxios";
+import useNotifications from "@/utils/hooks/useNotifications";
+import { useContext } from "react";
+
+export default function FormNote({
+    id,
+    isUpdate = false,
+    handleExit,
+}) {
+    const {onSuccess} = useNotifications();
+    const {userInfo} = useContext(AuthContext);
+
+    const preparePayload = (e) => {
+        e.preventDefault();
+        const userId = userInfo.userInfo.id;
+        const title = e.target.title.value;
+        const topic = e.target.topic.value;
+        const desc = e.target.description.value;
+        const payload = {
+            userId,
+            title,
+            topic,
+            desc,
+            updateDate: new Date().toLocaleDateString(),
+        };
+    
+        return payload;
+    };
+
+    const handleCreate = async (e) => {
+        const noteData = preparePayload(e);
+        const response = await useAxios('/notes', 'POST', noteData);
+
+        if(response) {
+            onSuccess("Notes added succesfully");
+            handleExit();
+        }
+    };
+
+    const handleSubmit = (e) => {
+        handleCreate(e);
+    };
+
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center bg-opacity-50 bg-black w-full min-h-[100vh] overflow-hidden top-0 left-0 z-50 fixed">
+                <div className="flex flex-col items-start justify-start bg-white rounded-2xl p-[30px] overflow-hidden relative max-h-[95vh]">
+                    <h4 className="text-[25px] font-bold -mt-[3px]">{isUpdate ? 'Update' : 'Create'} Note</h4>
+                    <form onSubmit={handleSubmit} className="flex flex-col items-start justify-start gap-[13px] mt-[14px] mb-[1px]">
+                        <div className="flex flex-col relative w-[600px]">
+                            <input
+                                type="text"
+                                className=" bg-white border-[1px] border-slate-300 rounded-[10px] w-full py-[5px] px-[9px] mt-[2px] focus:border-[4px] outline-none text-[22px] font-semibold"
+                                placeholder="Title"
+                                name="title"
+                            />
+                        </div>
+                        <div className="flex flex-col relative w-[600px]">
+                            <input
+                                type="text"
+                                className=" bg-white border-[1px] border-slate-300 rounded-[10px] w-full py-[5px] px-[9px] mt-[2px] focus:border-[4px] outline-none text-[18px] font-medium"
+                                placeholder="Topic"
+                                name="topic" 
+                            />
+                        </div>
+                        <div className="flex flex-col relative w-[600px]">
+                            <textarea
+                                className=" bg-white border-[1px] border-slate-300 rounded-[10px] w-full py-[5px] px-[9px] mt-[2px] focus:border-[4px] outline-none min-h-[100px] max-h-[300px]"
+                                placeholder="Description"
+                                name="description"
+                            />
+                        </div>
+                        <div className="flex flex-row items-center justify-end gap-[20px] w-full mt-[20px]">
+                            <Button
+                                text="Cancel"
+                                type="secondary"
+                                size="sm"
+                                onClick={handleExit}
+                            />
+                            <Button text={isUpdate ? "Update Note": "Add Note"} type="primary" size="sm" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ frontend/components/Sidebar.js
@@ -0,0 +1,38 @@
+import { AuthContext } from "@/utils/context/AuthContext";
+import { useContext } from "react";
+import Link from "next/link";
+import { FaTasks } from "react-icons/fa";
+import { CgNotes, CgProfile } from "react-icons/cg";
+import { TbLogout2 } from "react-icons/tb";
+
+export default function Sidebar() {
+
+    const {logoutUser} = useContext(AuthContext);
+
+    return (
+        <>
+            <div className="fixed flex flex-col items-center justify-between w-[110px] h-[100vh] bg-[#2984C9]">
+                <div className="flex flex-col items-start justify-start gap-[20px] mt-[30px]">
+                <div className="text-[35px] hover:bg-[#2b587a] active:bg-[#1e3c53] transition-all ease-in-out duration-200 hover:scale-110 rounded-lg p-[15px] cursor-pointer">
+                        <Link href="/todos">
+                            <FaTasks color="white" />
+                        </Link>
+                    </div>
+                    <div className="text-[35px]  hover:bg-[#2b587a] active:bg-[#1e3c53] transition-all ease-in-out duration-200 hover:scale-110 rounded-lg p-[15px] cursor-pointer">
+                        <Link href="/notes">
+                            <CgNotes color="white" />
+                        </Link>
+                    </div>
+                    <div className="text-[35px] bg-[#1e3c53] transition-all ease-in-out duration-200 hover:scale-110 rounded-lg p-[15px] cursor-pointer">
+                        <Link href="/">
+                            <CgProfile color="white" />
+                        </Link>
+                    </div>
+                </div>
+                <div className="text-[35px] hover:bg-[#2b587a] active:bg-[#1e3c53] transition-all ease-in-out duration-300 hover:scale-110 rounded-lg p-[15px] cursor-pointer mb-[30px]">
+                    <TbLogout2 color="white" onClick={logoutUser}/>
+                </div>
+            </div>
+        </>
+    );
+}
