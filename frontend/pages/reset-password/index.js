edba2a66548a0ce3cb4d7bad761e1ebc9811b5fa
imelnath <fiomelnathany@gmail.com>
2023-11-09 00:25:22 +0700

    feat: add return to login

--- frontend/pages/reset-password/index.js
+++ frontend/pages/reset-password/index.js
@@ -1,9 +1,12 @@
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import { AuthContext } from "@/utils/context/AuthContext";
+import { useRouter } from "next/router";
 import { useContext, useState } from "react";
 
 export default function ResetPassword() {
+    const router = useRouter();
+
     return (
         <div className="flex flex-col lg:flex-row items-center justify-center min-h-screen overflow-hidden relative mx-[35px] sm:mx-[40px] lg:mx-0">
             <div className="flex flex-col items-center justify-center w-full h-full lg:ml-[60px]">
@@ -33,6 +36,15 @@ export default function ResetPassword() {
                         className="w-full mt-[40px]"
                     />
                 </form>
+                <h3 className="text-[17px] sm:text-[19px] text-center mt-[30px] mx-[20px] sm:mx-0">
+                    Return to{" "}
+                    <span
+                        onClick={() => router.push("/login")}
+                        className="underline cursor-pointer text-[#2984C9] hover:text-[#2471AB]"
+                    >
+                        Login
+                    </span>
+                </h3>
             </div>
             <div className="hidden lg:flex flex-col items-center justify-center w-full h-full">
                 <img src="/assets/images/ResetPassword/hero.svg" className="" />
