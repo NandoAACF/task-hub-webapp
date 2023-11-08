ac9fd3ac617d1f99a15a20c5d48db0239e2dafe0
imelnath <fiomelnathany@gmail.com>
2023-11-09 00:40:17 +0700

    feat: implement responsive image

--- frontend/pages/reset-password/index.js
+++ frontend/pages/reset-password/index.js
@@ -11,10 +11,7 @@ export default function ResetPassword() {
         <div className="flex flex-col lg:flex-row items-center justify-center min-h-screen overflow-hidden relative mx-[35px] sm:mx-[40px] lg:mx-0">
             <div className="flex flex-col items-center justify-center w-full h-full lg:ml-[60px]">
                 <h1 className="text-[42px] sm:text-[52px] md:text-[60px] font-semibold text-center -mt-[20px]">
-                    Reset{" "}
-                    <span className="bg-gradient-to-r from-[#2984C9] via-[#3681B8] to-[#0B3654] text-transparent bg-clip-text">
-                        Password
-                    </span>
+                    Reset Password{" "}
                 </h1>
                 <form className="flex flex-col items-start justify-center w-full h-full gap-[14px] md:pl-[15%] md:pr-[15%] pl-[10%] pr-[10%] relative mt-[35px]">
                     <Input
@@ -45,6 +42,12 @@ export default function ResetPassword() {
                         Login
                     </span>
                 </h3>
+            </div>
+            <div className="lg:hidden order-first flex flex-col items-center justify-center w-full h-full">
+                <img
+                    src="/assets/images/ResetPassword/hero.svg"
+                    className="w-full"
+                />
             </div>
             <div className="hidden lg:flex flex-col items-center justify-center w-full h-full">
                 <img src="/assets/images/ResetPassword/hero.svg" className="" />
