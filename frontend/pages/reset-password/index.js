60c2d6c315140a3d43c89eeb720be40d5a200c8d
imelnath <fiomelnathany@gmail.com>
2023-11-09 00:08:19 +0700

    feat: update text color

--- frontend/pages/reset-password/index.js
+++ frontend/pages/reset-password/index.js
@@ -8,12 +8,30 @@ export default function ResetPassword() {
         <div className="flex flex-col lg:flex-row items-center justify-center min-h-screen overflow-hidden relative mx-[35px] sm:mx-[40px] lg:mx-0">
             <div className="flex flex-col items-center justify-center w-full h-full lg:ml-[60px]">
                 <h1 className="text-[42px] sm:text-[52px] md:text-[60px] font-semibold text-center -mt-[20px]">
-                    Reset Password{" "}
+                    Reset{" "}
+                    <span className="bg-gradient-to-r from-[#2984C9] via-[#3681B8] to-[#0B3654] text-transparent bg-clip-text">
+                        Password
+                    </span>
                 </h1>
                 <form className="flex flex-col items-start justify-center w-full h-full gap-[14px] md:pl-[15%] md:pr-[15%] pl-[10%] pr-[10%] relative mt-[35px]">
-                    <Input label="Password" type="password" className="w-full" name="password" />
-                    <Input label="Confirm Password" type="password" className="w-full" name="passwordConfirmation" />
-                    <Button text="Reset Password" type="primary" size="md" className="w-full mt-[40px]" />
+                    <Input
+                        label="Password"
+                        type="password"
+                        className="w-full"
+                        name="password"
+                    />
+                    <Input
+                        label="Confirm Password"
+                        type="password"
+                        className="w-full"
+                        name="passwordConfirmation"
+                    />
+                    <Button
+                        text="Reset Password"
+                        type="primary"
+                        size="md"
+                        className="w-full mt-[40px]"
+                    />
                 </form>
             </div>
             <div className="hidden lg:flex flex-col items-center justify-center w-full h-full">
